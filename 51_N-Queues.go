567e89cbdb47b00b8261b9eb0b4e8f8f5049e10c
Tom Wang <resonance.silent@gmail.com>
2018-01-02 17:46:59 +0800

    add problem 51

--- 51_N-Queues.go
+++ 51_N-Queues.go
@@ -2,31 +2,64 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-/**
-	1. 初始化结果棋盘
-	2.
- */
-func solveNQueens(n int) (result [][]string) {
-	// 初始化
-	chesstable := make([]string, n)
-	for row := 0; row < n; row++ {
-		for col := 0; col < n; col++ {
-			chesstable[row] = chesstable[row] + "."
+func CanPlace(table *[]int, row int, col int) (can bool) {
+	// 列重复
+	// 对角线重复，两个位置的坐标相减绝对值  [row - i] == [col - table[i]]
+	for k, v := range *table {
+		if v == col {
+			return  false
+		}
+		if math.Dim(math.Abs(float64(row - k)), math.Abs(float64(col - v))) < 0.000001 {
+			return false
 		}
 	}
-	for i := 0; i < n; i++ {
-		result = append(result, chesstable)
+	return true
+}
+
+func QueueHelper(result *[][]string, table *[]int, row int) {
+	if row == len(*table) {
+		// 添加到结果
+		// 生成棋盘
+		chessTable := make([]string, row)
+		for i := 0; i < row; i++ {
+			for j := 0; j < row; j++ {
+				if (*table)[i] == j {
+					chessTable[i] = chessTable[i] + "Q"
+				} else {
+					chessTable[i] = chessTable[i] + "."
+				}
+			}
+		}
+		*result = append(*result, chessTable)
+		return
+	}
+	for col := 0; col < len(*table); col++ {
+		if CanPlace(table, row, col) {
+			// 放入
+			(*table)[row] = col
+			// 下一行
+			QueueHelper(result, table, row+1)
+		}
 	}
 }
 
+func solveNQueens(n int) (result [][]string) {
+	table :=  make([]int, n)
+	for i := 0; i < len(table); i++ {
+		table[i] = -1
+	}
+	QueueHelper(&result, &table, 0)
+	return result
+}
+
 func main() {
-	chesstable := make([]string, 5)
-	for row := 0; row < 5; row++ {
-		for col := 0; col < 5; col++ {
-			chesstable[row] = chesstable[row] + "."
+	result := solveNQueens(4)
+	for _, k := range result {
+		for _, k2 := range k {
+			fmt.Println(k2)
 		}
 	}
-	fmt.Println(chesstable)
 }
